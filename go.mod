523682a7a21cb20a6c0f660d6e91e00865b0a478
gogod3v <46003678+gogod3v@users.noreply.github.com>
2022-07-26 12:30:50 +0000

    some test unit tests

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/gogod3v/go-test-repo
 
 go 1.14
 
-require github.com/labstack/echo/v4 v4.1.17 // indirect
+require (
+	github.com/labstack/echo/v4 v4.1.17
+	github.com/stretchr/testify v1.4.0
+)
---
+++ main_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+	"math/rand"
+)
+
+func Test_success(t *testing.T) {
+	a := 1
+	b := 1
+	require.Equal(t, a, b, "The two words should be the same.")
+}
+
+func Test_fail(t *testing.T) {
+	a := 1
+	b := 2
+	require.Equal(t, a, b, "The two words should be the same - failing test.")
+}
+
+func Test_random(t *testing.T) {
+	s1 := rand.NewSource(time.Now().UnixNano())
+    r1 := rand.New(s1)
+	randNum := r1.Intn(100)
+
+	require.Equal(t, true, randNum < 50, "More than 50?")
+}
+
+func Test_long(t *testing.T) {
+	a := 1
+	b := 1
+	time.Sleep(2 * time.Second)
+	require.Equal(t, a, b, "Compare after 2 seconds")
+}
