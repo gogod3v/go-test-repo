cf2962b0ca076c3f8c771148d3f32aa17a5c865a
gogod3v <46003678+gogod3v@users.noreply.github.com>
2023-05-16 15:35:29 +0200

    Update install_bitrise_cli.sh

--- _scripts/install_bitrise_cli.sh
+++ _scripts/install_bitrise_cli.sh
@@ -3,7 +3,7 @@ set -ex
 
 export BITRISE_CLI_VERSION='2.2.5'
 
-curl -fL https://github.com/bitrise-io/bitrise/releases/download/$BITRISE_CLI_VERSION/bitrise-"$(uname -s)"-"$(uname -m)" > /tmp/bitrise
+curl -fL https://github.com/bitrise-io/bitrise/releases/download/$BITRISE_CLI_VERSION/bitrise-Linux-x86_64 > /tmp/bitrise
 sudo mv /tmp/bitrise /usr/local/bin/bitrise
 chmod +x /usr/local/bin/bitrise
 bitrise setup
